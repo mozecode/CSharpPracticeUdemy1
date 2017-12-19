6b3626874127ce583b9b696587055a0370cf7ef0
Greta Moseley <greta.moseley@gmail.com>
2017-12-19 12:16:16 -0600

    add exercise from Udemy practicing CSharp Conditionals

--- CSharpPracticeApps/Program.cs
+++ CSharpPracticeApps/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,30 @@ namespace CSharpPracticeApps
     {
         static void Main(string[] args)
         {
+            int chosen;
+            Console.WriteLine("Please enter a number between 1 and 10");
+
+            while (int.TryParse(Console.ReadLine(), out chosen) == false)
+            {
+             Console.WriteLine("Try again, please: ");   
+            }
+
+            if (chosen <= 0 || chosen > 10)
+            {
+                Console.WriteLine("Sorry, that number is invalid.");
+                Console.WriteLine("Please enter a number between 1 and 10");
+
+                while (int.TryParse(Console.ReadLine(), out chosen) == false)
+                {
+                    Console.WriteLine("Try again, please: ");
+                }
+            }
+            else if (chosen > 0 && chosen <= 10)
+            {
+                Console.WriteLine("Congrats.  You can read!");
+            }
+            
         }
+
     }
 }
